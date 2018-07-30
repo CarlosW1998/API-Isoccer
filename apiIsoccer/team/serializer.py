e3838afc19b137271e3634c8373c4ed277324b64
Carlos <carlos-walte@hotmail.com>
2018-07-30 16:23:01 -0300

    serializer and views

--- apiIsoccer/team/serializer.py
+++ apiIsoccer/team/serializer.py
@@ -1,1 +1,15 @@
 from rest_framework import serializers
+from team.models import employee, recourse, fans
+
+class employeeserializer(serializers.ModelSerializer) :
+    class meta :
+        meta = employee
+        fields = (__all__)
+class recourseSerializer(serializers.ModelSerializer) :
+    class meta :
+        meta = recourse
+        fields = (__all__)
+class fansSerializer(serializers.ModelSerializer) :
+    class meta :
+        meta = fans
+        fields = (__all__)
--- apiIsoccer/team/views.py
+++ apiIsoccer/team/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from team.serializer import employeeserializer, fansSerializer, recourseSerializer
+from team.models import employee, fans, recourse
+from rest_framework import Response, status, generics, permissions
 # Create your views here.
+
+class EmployeesList(generics.ListCreateAPIView) :
+    queryset = employee.objects.all()
+    serializer_class = employeeserializer
+class EmployeesDetail(generics.RetriveUpdateDestroyAPIView) :
+    queryset = employee.objects.all()
+    serializer_class = employeeserializer
+
+class RecourseList(generics.ListCreateAPIView) :
+    queryset = recourse.objects.all()
+    serializer_class = fansSerializer
+class RecourseDetail(generics.RetriveUpdateDestroyAPIView) :
+    queryset = recourse.objects.all()
+    serializer_class = fansSerializer
+
+class FansList(generics.ListCreateAPIView) :
+    queryset = fans.objects.all()
+    serializer_class = fansSerializer
+class FansDetail(generics.RetriveUpdateDestroyAPIView) :
+    queryset = fans.objects.all()
+    serializer_class = fansSerializer
